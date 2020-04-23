2241673e1ec54ee7b38a8d04c5822ebfa7f60b17
Hai Le <lehoanghai310@gmail.com>
2020-04-23 18:59:09 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -23,6 +23,7 @@ sudo apt-get install python-certbot-nginx -y
 sudo apt install php7.2-fpm php7.2-common php7.2-mysql php7.2-gmp php7.2-curl php7.2-intl php7.2-mbstring php7.2-xmlrpc php7.2-gd php7.2-xml php7.2-cli php7.2-zip -y
 sudo apt install wget -y
 sudo apt install unzip -y
+sudo apt install expect -y
 
 
 mysql -u root <<-EOF
